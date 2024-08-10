a49e2dace6ceadbe29471e73362405a427555226
utidanet <utidanet23@gmail.com>
2024-08-10 21:08:24 +0900

    test01

---
+++ Webpack.config.js
@@ -0,0 +1,28 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.ts',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  target: 'electron-renderer',
+};
---
+++ src/renderer.tsx
@@ -0,0 +1,8 @@
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+const App = () => {
+  return <div>Hello, Electron with React and TypeScript!</div>;
+};
+
+ReactDOM.render(<App />, document.getElementById('root'));
